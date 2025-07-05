e778f26d2795e29c8c2b277ebc7c371f664ff731
Fabián Chacón <115729645+fchaconb@users.noreply.github.com>
2025-07-04 20:14:55 -0600

    fix: added school entity and controller, updated user entity to include school and modified the admin seeder to include the default school

---
+++ src/main/java/com/project/demo/rest/school/SchoolRestController.java
@@ -0,0 +1,81 @@
+package com.project.demo.rest.school;
+
+import com.project.demo.logic.entity.http.GlobalResponseHandler;
+import com.project.demo.logic.entity.http.Meta;
+import com.project.demo.logic.entity.school.School;
+import com.project.demo.logic.entity.school.SchoolRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/schools")
+public class SchoolRestController {
+
+    @Autowired
+    private SchoolRepository schoolRepository;
+
+    @GetMapping
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> getAll(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size,
+            HttpServletRequest request) {
+
+        Pageable pageable = PageRequest.of(page-1, size);
+        Page<School> schoolPage = schoolRepository.findAll(pageable);
+        Meta meta = new Meta(request.getMethod(), request.getRequestURL().toString());
+        meta.setTotalPages(schoolPage.getTotalPages());
+        meta.setTotalElements(schoolPage.getTotalElements());
+        meta.setPageNumber(schoolPage.getNumber() + 1);
+        meta.setPageSize(schoolPage.getSize());
+
+        return new GlobalResponseHandler().handleResponse("Schools retrieved successfully",
+                schoolPage.getContent(), HttpStatus.OK, meta);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    public ResponseEntity<?> addSchool(@RequestBody School school, HttpServletRequest request) {
+        schoolRepository.save(school);
+        return new GlobalResponseHandler().handleResponse("School created successfully",
+                school, HttpStatus.OK, request);
+    }
+
+    @PutMapping("/{schoolId}")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    public ResponseEntity<?> updateSchool(@PathVariable Long schoolId, @RequestBody School school, HttpServletRequest request) {
+        Optional<School> foundSchool = schoolRepository.findById(schoolId);
+        if (foundSchool.isPresent()) {
+            School schoolUpdated = schoolRepository.save(school);
+            return new GlobalResponseHandler().handleResponse("School updated sucessfully",
+                    schoolUpdated, HttpStatus.OK, request);
+        } else {
+            return new GlobalResponseHandler().handleResponse("School id " + schoolId + " not found",
+                    HttpStatus.NOT_FOUND, request);
+        }
+    }
+
+    @DeleteMapping("/{schoolId}")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    public ResponseEntity<?> deleteSchool(@PathVariable Long schoolId, HttpServletRequest request) {
+        Optional<School> foundSchool = schoolRepository.findById(schoolId);
+        if (foundSchool.isPresent()) {
+            schoolRepository.deleteById(schoolId);
+            return new GlobalResponseHandler().handleResponse("School deleted successfully",
+                    foundSchool.get(), HttpStatus.OK, request);
+        } else {
+            return new GlobalResponseHandler().handleResponse("School " + schoolId + " not found",
+                    HttpStatus.NOT_FOUND, request);
+        }
+    }
+
+}
