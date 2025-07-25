fc6579d645c948f5bcf9126f0ce94e5cce5d0087
AndresSC <aseguracalderon26@gmail.com>
2025-07-24 22:41:37 -0600

    group changed

--- src/main/java/com/project/demo/logic/entity/group/GroupRepository.java
+++ src/main/java/com/project/demo/logic/entity/group/GroupRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
     Page<Group> findGroupsByCourseId(Long courseId, Pageable pageable);
 
     Page<Group> findGroupsByTeacherId(Long teacherId, Pageable pageable);
+
+    Optional<Group> findWithStudentsById(Long groupId);
 }
