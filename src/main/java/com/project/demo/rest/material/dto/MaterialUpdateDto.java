ae365c1f033be0a04e0225b4b653cc1d698dbc20
Andrés Segura Calderón <119067676+AndresSC25@users.noreply.github.com>
2025-07-31 23:46:44 -0600

    dto for materials (#52)
    * dto for materials
    
    * Removed comments
    
    ---------
    
    Co-authored-by: Fabián Chacón <115729645+fchaconb@users.noreply.github.com>

---
+++ src/main/java/com/project/demo/rest/material/dto/MaterialUpdateDto.java
@@ -0,0 +1,44 @@
+package com.project.demo.rest.material.dto;
+
+public class MaterialUpdateDto {
+
+    private String name;
+    private String fileUrl;
+    private Long courseId;
+    private Long teacherId;
+
+    public MaterialUpdateDto() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    public Long getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Long teacherId) {
+        this.teacherId = teacherId;
+    }
+}
